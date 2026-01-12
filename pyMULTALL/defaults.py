cfc5123315e59cb32d2d65c63c7a5842925b0a98
Andrea Pinardi <andreapinardi319@gmail.com>
2026-01-12 17:03:40 +0100

    Add pyMULTALL's package path

---
+++ pyMULTALL/defaults.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Default values for global pyMULTALL parameters.
+
+@author: Andrea Pinardi <andrea.pinardi@polimi.it>
+"""
+
+from pathlib import Path
+
+# absolute path of the pyMULTALL package directory
+PYMULTALL = Path(__file__).parent.resolve()
