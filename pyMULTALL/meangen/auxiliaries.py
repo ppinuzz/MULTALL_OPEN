e5c00be1884f4aa06e9e881599a555ea7f1b1913
Andrea Pinardi <andreapinardi319@gmail.com>
2026-01-24 18:26:30 +0100

    Add all user input for axial flow stages

--- pyMULTALL/meangen/auxiliaries.py
+++ pyMULTALL/meangen/auxiliaries.py
@@ -59,7 +59,7 @@ def print_startup_message(len_separator=70):
     print(f'Thermodynamic backend (for real gas model) : {pyMULTALL.defaults.THERMO_BACKEND}')
     if pyMULTALL.defaults.THERMO_BACKEND == 'REFPROP':
         if pyMULTALL.defaults.REFPROP_PATH is None:
-            aux.print_message("No environmental variable 'RPPREFIX' was found, CoolProp may not be able to load REFPROP", aux.MessageLevel.BASIC)
+            aux.print_message("No environmental variable 'RPPREFIX' was found, CoolProp may not be able to load REFPROP", aux.MessageLevel.WARNING)
         else:
             print(f'REFPROP path : {pyMULTALL.defaults.REFPROP_PATH}')
     print(separator)
@@ -120,9 +120,10 @@ def interactive_input():
             raise ValueError(f"Unknown machine flow type '{flow_type}'")
     input_data['machine_flow_type'] = flow_type
     
-    print('Sign conventions: ')
-    print('- The blade rotation must in the positive theta direction')
-    print('- Flow angles are positive is the associated velocity vector has a tangential component in the positive theta direction (i.e. positive rotation direction)')
+    aux.print_message('Sign conventions: '
+          '\n\t - The blade rotation must in the positive theta direction'
+          '\n\t - Flow angles are positive is the associated velocity vector has a tangential component in the positive theta direction (i.e. positive rotation direction)',
+          aux.MessageLevel.INFO)
     
     input_data['inlet_conditions'] = {}
     p_tot_in = float(input('Inlet stagnation pressure [bar]: '))
@@ -220,7 +221,7 @@ def interactive_input():
     input_data['stages'] = {i: None for i in range(1, N_stages+1)}
     for i in range(1, N_stages+1):
         stage_data = {}
-        print(f'Starting stage number {i}')
+        aux.print_message(f'Starting stage number {i}', aux.MessageLevel.INFO)
         
         if i > 1:
             equal_stage = print('Are the angles, mass flow rate, design radius, isentropic efficiency, etc. for this stage the same as for the previous stage [y/n]?')
@@ -257,11 +258,13 @@ def interactive_input():
         
         
         # MIXED FLOW STAGE PARAMETERS
-        if flow_type == 'mixed':
-            input_method = input('For MIXED flow machines, two input methods are available: '
-                                 '\n\t A) input all 4 blade angles'
-                                 '\n\t B) input the absolute flow angles at stage inlet and outlet and the flow coefficient (phi) and stage loading coefficient (psi)'
-                                 '\n Design method [a/b]: ')
+        if change_flow_stage and stage_flow_type == 'mixed':
+            aux.print_message('For MIXED flow machines, two input methods are available: '
+                              '\n\t A) input all 4 blade angles'
+                              '\n\t B) input the absolute flow angles at stage inlet and outlet and the flow coefficient (phi) and stage loading coefficient (psi)', 
+                              aux.MessageLevel.INFO)
+            
+            input_method = input('Design method [a/b]: ')
             match input_method.lower():
                 case 'a':
                     input_method = 'blade_angles'
@@ -321,7 +324,8 @@ def interactive_input():
             change_stream_surf_coords = True
             while change_stream_surf_coords:
                 print('Input the stream surface coordinates and the meridional velocity ratios')
-                print('The new values must form a smooth continuation of the last stream surface')
+                aux.print_message('The new values must form a smooth continuation of the last stream surface', 
+                                  aux.MessageLevel.INFO)
                 if i == 0:
                     N_pts_stream_surf = float(input('Number of points (i.e. axial coordinates) on the mean stream surface: '))
                 else:
@@ -350,7 +354,8 @@ def interactive_input():
                             axial_stream_coords = list(map(float, axial_stream_coords))
                         case _:
                             raise ValueError(f"Unknown answer '{use_old_axial_stream_coords}'")
-                
+                    stage_data['stream_surf_axial_coords'] = axial_stream_coords.copy()
+                    
                 if i > 0:
                     print(f"Radial coordinates of the stream surface of the previous stage [m]: {input_data['stages'][i-1]['stream_surf_radial_coords']}")
                     use_old_radial_stream_coords = input('Use the radial stream surface coordinates of the previous stage also in this stage [y/n]? ')
@@ -364,7 +369,8 @@ def interactive_input():
                             radial_stream_coords = list(map(float, radial_stream_coords))
                         case _:
                             raise ValueError(f"Unknown answer '{use_old_radial_stream_coords}'")
-                
+                    stage_data['stream_surf_radial_coords'] = radial_stream_coords.copy()
+                    
                 if i > 0:
                     print(f"Meridional velocity ratios (V_merid/V_merid@1st rotor LE) on the stream surface of the previous stage: {input_data['stages'][i-1]['meridional_velocity_ratios']}")
                     use_old_merid_vel_ratios = input('Use the meridional velocity ratios of the previous stage also in this stage [y/n]? ')
@@ -378,7 +384,8 @@ def interactive_input():
                             meridional_velocity_ratios = list(map(float, meridional_velocity_ratios))
                         case _:
                             raise ValueError(f"Unknown answer '{use_old_merid_vel_ratios}'")
-                
+                    stage_data['meridional_velocity_ratios'] = meridional_velocity_ratios.copy()
+                    
                 if i > 0:
                     print('"Indices of the leading and trailing edge of the previous stage (starting from index 1 on the mean stream surface): \n'
                           "\t Leading edge blade 1: point {input_data['stages'][i-1]['LE_TE_mean_stream_surf'][0]]}"
@@ -399,6 +406,7 @@ def interactive_input():
                             raise ValueError(f"Unknown answer '{use_old_LE_TE_idx}'")
                 if len(idx_LE_TE_mean_stream) != 4:
                     raise ValueError('You must input 4 values: LE blade 1, TE blade 1, LE blade 2, TE blade 2')
+                stage_data['LE_TE_mean_stream_surf'] = idx_LE_TE_mean_stream.copy()
                 
                 # MESH RECAP
                 # for the table only: list of empty strings with 4 labels
@@ -428,12 +436,189 @@ def interactive_input():
                     case _:
                         raise ValueError(f"Unknown answer '{change_stream_surf_coords}'")
                 stage_data['change_stream_surf_coords'] = change_stream_surf_coords
+        elif change_flow_stage and stage_flow_type == 'axial':
+            aux.print_message('For an axial stage, there are 3 possible set of inputs you can use to specify the velocity triangles: '
+                              '\n\t A) reaction degree, flow coefficient and stage loading coefficient'
+                              '\n\t B) flow coefficient, stator exit angle and rotor exit angle'
+                              '\n\t C) flow coefficient, rotor inlet angle and rotor exit angle'
+                              '\n\t D) reaction degree, first blade row inlet angle and first blade row exit angle', 
+                              aux.MessageLevel.INFO)
+            input_method = input('Choose an input method [a/b/c/d]: ')
+            match input_method.lower():
+                case 'a':
+                    input_method = 'chi_phi_psi'
+                case 'b':
+                    input_method = 'phi_statout_rotout'
+                case 'c':
+                    input_method = 'phi_rotin_rotout'
+                case 'd':
+                    input_method = 'chi_rowin_rowout'
+                case _:
+                    raise ValueError("Unknown input method '{input_method}'")
+            stage_data['velocity_triangles_method'] = input_method
+            
+            match input_method:
+                case 'chi_phi_psi':
+                    chi = float(input('Reaction degree: '))
+                    if chi < 0 or chi > 1:
+                        aux.print_message('While technically not impossible, depending on the definition, having a rection degree < 0 or > 1 is probably wrong...', aux.MessageLevel.WARNING)
+                    stage_data['reaction_degree'] = chi
+                    
+                    phi = float(input('Flow coefficient: '))
+                    if phi < 0:
+                        raise ValueError('Flow coefficient cannot be zero or negative')
+                    stage_data['flow_coeff'] = phi
+                    
+                    psi = float(input('Stage loading coefficient: '))
+                    if psi < 0:
+                        raise ValueError('Stage loading coefficient cannot be zero or negative')
+                    stage_data['stage_load_coeff'] = psi
+                case 'phi_statout_rotout':
+                    phi = float(input('Flow coefficient: '))
+                    if phi < 0:
+                        raise ValueError('Flow coefficient cannot be zero or negative')
+                    stage_data['flow_coeff'] = phi
+                    
+                    # TODO: non ho capito se siano angoli assoluti o relativi
+                    # per il rotore...
+                    angle_stat_out = float(input('Stator exit angle [deg]: '))
+                    if abs(angle_stat_out) > 90:
+                        raise ValueError('All angles must lie in the range [-90, +90] deg')
+                    stage_data['angle_stat_out'] = angle_stat_out
+                    
+                    angle_rot_out = float(input('Rotor exit angle [deg]: '))
+                    if abs(angle_rot_out) > 90:
+                        raise ValueError('All angles must lie in the range [-90, +90] deg')
+                    stage_data['angle_rot_out'] = angle_rot_out
+                case 'phi_rotin_rotout':
+                    phi = float(input('Flow coefficient: '))
+                    if phi < 0:
+                        raise ValueError('Flow coefficient cannot be zero or negative')
+                    stage_data['flow_coeff'] = phi
+                    
+                    # TODO: non ho capito se siano angoli assoluti o relativi
+                    # per il rotore...
+                    angle_rot_in = float(input('Rotor inlet angle [deg]: '))
+                    if abs(angle_rot_in) > 90:
+                        raise ValueError('All angles must lie in the range [-90, +90] deg')
+                    stage_data['angle_rot_in'] = angle_rot_in
+                    
+                    angle_rot_out = float(input('Rotor exit angle [deg]: '))
+                    if abs(angle_rot_out) > 90:
+                        raise ValueError('All angles must lie in the range [-90, +90] deg')
+                    stage_data['angle_rot_out'] = angle_rot_out
+                case 'chi_rowin_rowout':
+                    chi = float(input('Reaction degree: '))
+                    if chi < 0 or chi > 1:
+                        aux.print_message('While technically not impossible, depending on the definition, having a rection degree < 0 or > 1 is probably wrong...', aux.MessageLevel.WARNING)
+                    stage_data['reaction_degree'] = chi
+                    
+                    angle_row_in = float(input('First blade row inlet angle [deg]: '))
+                    if abs(angle_row_in) > 90:
+                        raise ValueError('All angles must lie in the range [-90, +90] deg')
+                    stage_data['angle_row_in'] = angle_row_in
+                    
+                    angle_row_out = float(input('First blade row exit angle [deg]: '))
+                    if abs(angle_row_out) > 90:
+                        raise ValueError('All angles must lie in the range [-90, +90] deg')
+                    stage_data['angle_row_out'] = angle_row_out
+            
+            aux.print_message('For an axial stage, there are 2 ways to set the design radius:'
+                  '\n\t A) input the design radius directly'
+                  '\n\t B) input the stage enthalpy change', 
+                              aux.MessageLevel.INFO)
+            design_radius_method = input('Method to set the design radius [a/b]: ')
+            match design_radius_method.lower():
+                case 'a':
+                    design_radius_method = 'set_radius'
+                case 'b':
+                    design_radius_method = 'set_enthalpy_change'
+                case _:
+                    raise ValueError(f"Unknown method '{design_radius_method}'")
+            stage_data['design_radius_method'] = design_radius_method
+            
+            match design_radius_method:
+                case 'set_radius':
+                    R_design = float(input('Design point radius [m]: '))
+                    if R_design <= 0:
+                        raise ValueError('A radius cannot be zero or negative')
+                    stage_data['design_radius'] = R_design
+                case 'set_enthalpy_change':
+                    dh_stage = float(input('Stage actual enthalpy change (absolute value) [kJ/kg]: '))
+                    # TODO: il caso con dh = 0 va escluso o considerato?
+                    if dh_stage < 0:
+                        raise ValueError('The stage actual enthalpy change must be provided in modulus, no matter if it is an increase or a decrease in enthalpy')
+                    # [kJ/kg] -> [J/kg]
+                    stage_data['actual_enthalpy_change'] = 1e3 * dh_stage
+            
+            # TODO: non so dove vadano questi! sono solo per il caso assiale??
+            aux.print_message('Default axial blade chords:'
+                              f'\n\t Row 1: {meangen_def.c_ax_1}'
+                              f'\n\t Row 2: {meangen_def.c_ax_2}',
+                              aux.MessageLevel.INFO)
+            change_c_ax = input('Change the axial blade chords default values [y/n]? ')
+            match change_c_ax.lower():
+                case 'y':
+                    change_c_ax = True
+                case 'n':
+                    change_c_ax = False
+                case _:
+                    raise ValueError(f"Unknown answer '{change_c_ax}'")
+            
+            if change_c_ax:
+                c_ax_1 = float(input('First blade row axial chord [m]: '))
+                if c_ax_1 <= 0:
+                    # actually, it could be zero, but then the blade is blocking the meridional channel
+                    raise ValueError('An (axial) blade chord cannot be zero or negative')
+                stage_data['axial_chord_1'] = c_ax_1
+                
+                c_ax_2 = float(input('Second blade row axial chord [m]: '))
+                if c_ax_2 <= 0:
+                    # actually, it could be zero, but then the blade is blocking the meridional channel
+                    raise ValueError('An (axial) blade chord cannot be zero or negative')
+                stage_data['axial_chord_2'] = c_ax_2
+            else:
+                stage_data['axial_chord_1'] = meangen_def.c_ax_1
+                stage_data['axial_chord_2'] = meangen_def.c_ax_2
+            
+            aux.print_message('Default inter-row gap and inter-stage gap as fractions of the first row axial chord (gap/c_ax_1):'
+                              f'\n\t Inter-row gap/axial_chord_1: {meangen_def.row_gap2cax}'
+                              f'\n\t Inter-stage gap/axial_chord_1: {meangen_def.stage_gap2cax}',
+                              aux.MessageLevel.INFO)
+            change_axial_gaps = input('Change the inter-row and inter-stage axial gaps default values [y/n]? ')
+            match change_axial_gaps.lower():
+                case 'y':
+                    change_axial_gaps = True
+                case 'n':
+                    change_axial_gaps = False
+                case _:
+                    raise ValueError(f"Unknown answer '{change_axial_gaps}'")
+
+            if change_axial_gaps:
+                row_gap = float(input('Inter-row gap/axial_chord_1: '))
+                if row_gap <= 0:
+                    # it could be zero in a simplified model (?)
+                    raise ValueError('The non-dimensional inter-row gap cannot be zero or negative')
+                stage_data['row_gap2cax'] = row_gap
+                
+                stage_gap = float(input('Inter-stage gap/axial_chord_1: '))
+                if stage_gap <= 0:
+                    # it could be zero in a simplified model (?)
+                    raise ValueError('The non-dimensional inter-stage gap cannot be zero or negative')
+                stage_data['stage_gap2cax'] = stage_gap
+            else:
+                stage_data['row_gap2cax'] = meangen_def.row_gap2cax
+                stage_data['stage_gap2cax'] = meangen_def.stage_gap2cax
+            
         
-        # DATA FOR BOT AXIAL AND MIXED FLOW TYPE
-        print('NB: blockage factor = sum of the hub and casing boundary layer displacement thicknesses, divided by the blade span')
-        print('Default values: \n'
+        # ---------- DATA FOR BOTH AXIAL AND MIXED FLOW TYPE ----------
+        aux.print_message('NB: blockage factor = sum of the hub and casing boundary layer displacement thicknesses, divided by the blade span',
+                          aux.MessageLevel.INFO)
+        aux.print_message('Default values: \n'
               f'\t BL at LE of 1st row: {meangen_def.blockageLE_1} \n'
-              f'\t BL at TE of 2nd row: {meangen_def.blockageTE_2}')
+              f'\t BL at TE of 2nd row: {meangen_def.blockageTE_2}',
+                          aux.MessageLevel.INFO)
+        
         change_BF = input('Change the blockage factor default values [y/n]? ')
         match change_stream_surf_coords.lower():
             case 'y':
@@ -459,7 +644,6 @@ def interactive_input():
             stage_data['blockage_factor_LE_first'] = meangen_def.blockageLE_1
             stage_data['blockage_factor_TE_last'] = meangen_def.blockageTE_2
         
-            
             
         # .copy() to avoid shallow copying it and changing it
         input_data['stages'][i] = stage_data.copy()
